f40964032350f7f7836bd31eb67da95491297f27
GuilhermeMViana <gmunizviana@gmail.com>
2026-02-23 11:31:30 -0300

    Initial Commit

---
+++ Musics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Musics
+//
+//  Created by Guilherme Muniz Viana on 23/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Musics/MusicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicsApp.swift
+//  Musics
+//
+//  Created by Guilherme Muniz Viana on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
